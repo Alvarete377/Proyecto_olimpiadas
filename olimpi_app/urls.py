9498cf74236158151202a997a9be1809aedf1b58
Alvarete377 <tezz17.com@gmail.com>
2025-10-28 21:20:03 +0100

    DetailsView

--- olimpi_app/urls.py
+++ olimpi_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from .views import EncuentrosListView
-
+from .views import EncuentrosListView, EncuentrosDetailView
 urlpatterns = [
     path("partidos/", EncuentrosListView.as_view(), name="partidos"),
+    path("detalles/<int:pk>/", EncuentrosDetailView.as_view(), name="detalles")
 ]
--- olimpi_app/views.py
+++ olimpi_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.core.exceptions import ValidationError
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Encuentros, Disciplinas, Pistas, Arbitros, Equipos, EncuentrosEquipos
 
 # Vista basada en clase para listar encuentros (ListView)
@@ -10,3 +10,14 @@ class EncuentrosListView(ListView):
     model = Encuentros
     context_object_name = 'encuentros'
 
+class EncuentrosDetailView(DetailView):
+    model = Encuentros
+    context_object_name = 'encuentro'
+    
+    def get_queryset(self):
+        # Incluye todas las relaciones para optimizar consultas
+        return Encuentros.objects.select_related(
+            'idDis', 'idPis', 'idArb'
+        ).prefetch_related(
+            'encuentrosequipos_set__equipo'
+        )
